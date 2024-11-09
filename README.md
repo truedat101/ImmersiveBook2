c4b8348e86feea9ecea6d75f943eec1faf9fd7a2
David J. kordsmeier <dkords@gmail.com>
2024-11-09 02:42:43 -0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# Overview
+
+This is the Immersive Book 2.0 project, an Immerse The Bay 2024 team project.  Original project idea is described here: https://qiita.com/truedata_iotone/items/e73aa5dc98a9fba46a60#book20---a-rethinking-of-books-in-xr
+
+## License
+
+MIT/X
+
+## Team
+
+TODO
+
+## Architecture
+
+TODO
+
+### Hardware
+
+- Meta Quest 3(s)
+- Chrome Browser/PC/Mac/Linux
+
+## Code
+
+### Src Layout
+
+- src : the final source code
+- assets: a JSON file containing links to any static assets
+- pipeline: this is the generative AI pipeline in combination with the graphics pipeline
+- prototypes: these are early prototypes that may later be thrown out or kept for historical reasons
+
+### Building
+
+TODO
+
+### Running
+
+TODO
+
+### Demo
+
+- TODO
+
+## Attributions
+
+- Elixr Prototype: https://elixrjs.io/index.html
+- Meta WebXR example for Quest: https://github.com/meta-quest/webxr-first-steps
+- AFrame examples: https://glitch.com/~aframe-basic-guide
+
+## Inspirations
+
+
+- AR.js prototype: https://ar-js-org.github.io/AR.js-Docs/
+- min
---
+++ prototypes/elixr-poc1/index.js
@@ -0,0 +1,95 @@
+import {
+	AmbientLight,
+	Color,
+	DirectionalLight,
+	GameSystem,
+	Mesh,
+	MeshStandardMaterial,
+	PhysicsMaterial,
+	PlaneCollider,
+	PlaneGeometry,
+	RigidbodyType,
+	SphereCollider,
+	SphereGeometry,
+	VRButton,
+	initEngine,
+} from 'elixr';
+
+		// url: 'https://elysian.fun/assets/gltf/props.gltf',
+const asset1 = 'https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/refs/heads/main/2.0/BoomBox/glTF/BoomBox.gltf';
+const asset2 = 'https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/refs/heads/main/2.0/AnimatedCube/glTF/AnimatedCube.gltf';
+const asset3 = 'https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Assets/refs/heads/main/Models/BoxTextured/glTF-Embedded/BoxTextured.gltf';
+
+const assets = {
+	props: {
+		url: asset3,
+		type: 'GLTF',
+		callback: (gltf) => {
+			console.log(gltf);
+		},
+	},
+};
+
+class ExampleSystem extends GameSystem {
+	init() {
+		const pmat = new PhysicsMaterial({ friction: 1, restitution: 1 });
+		const sphere = this.createRigidbody()
+			.add(new SphereCollider(1, false, pmat))
+			.add(
+				new Mesh(
+					new SphereGeometry(1, 32, 32),
+					new MeshStandardMaterial({ color: 0xff0000 }),
+				),
+			);
+		sphere.position.set(0, 5, 0);
+		sphere.updateTransform();
+		sphere.colliderVisible = true;
+		this.scene.add(sphere);
+		this.floor = this.createRigidbody({ type: RigidbodyType.Kinematic })
+			.add(new PlaneCollider(10, 10, false, pmat))
+			.add(
+				new Mesh(
+					new PlaneGeometry(10, 10),
+					new MeshStandardMaterial({ color: 0x00ff00 }),
+				),
+			);
+		this.floor.position.set(0, -1, 0);
+		this.floor.rotateX(-Math.PI / 2);
+		this.floor.colliderVisible = true;
+	}
+
+	update(delta) {
+		this.floor.position.y += delta;
+		if (this.floor.position.y > 1) {
+			this.floor.position.y = -1;
+		}
+		// console.log(this.sphereRigidBody.parent.position.toArray());
+	}
+}
+
+initEngine(
+	document.getElementById('scene-container'),
+	{ enablePhysics: true },
+	assets,
+).then((world) => {
+	world.registerSystem(ExampleSystem);
+	world.scene.background = new Color(0x000000);
+
+	// Add ambient light
+	const ambientLight = new AmbientLight(new Color(0xffffff), 1);
+	world.scene.add(ambientLight);
+
+	world.scene.add(assets);
+	// Add directional light
+	const directionalLight = new DirectionalLight(new Color(0xffffff), 0.5);
+	directionalLight.position.set(0, 1, 0);
+	world.scene.add(directionalLight);
+
+	// Set camera position
+	world.camera.position.set(0, 0, 10);
+	console.log(world.camera.position);
+	globalThis.world = world;
+
+	const vrButton = document.getElementById('vr-button');
+	VRButton.convertToVRButton(vrButton, world.renderer);
+});
